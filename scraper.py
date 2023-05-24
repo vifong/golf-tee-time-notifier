fad5ec1cb15ec6fba7fbf9fbc90970c0132dab14
vifong <vifong@github.com>
2023-05-24 13:37:54 -0700

    testing

--- scraper.py
+++ scraper.py
@@ -1,11 +1,13 @@
 from collections import defaultdict
-from typing import NamedTuple
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from typing import List
+from typing import NamedTuple
+from typing import Tuple
 import argparse
 import datetime
 import re
