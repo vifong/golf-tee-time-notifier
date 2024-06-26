d9b7641fd709143087d1c79ac73bbe80752a1e68
Vivian Fong <vifong@users.noreply.github.com>
2024-06-26 09:25:18 -0700

    Update main.py

--- golfteetimenotifier/main.py
+++ golfteetimenotifier/main.py
@@ -21,9 +21,9 @@ COURSES = [
     GolfCourse('Rancho Park Golf Course', 'rancho-park-golf-course', '12203'),
     GolfCourse('Woodley Lakes Golf Course', 'woodley-lakes-golf-course', '12205'),
     # GolfCourse('Balboa Golf Course', 'balboa-golf-course', '12197'),
-    # GolfCourse('Encino Golf Course', 'encino-golf-course', '12200'),
+    GolfCourse('Encino Golf Course', 'encino-golf-course', '12200'),
     # GolfCourse('Penmar Golf Course', 'penmar-golf-course', '12219'),
-    GolfCourse('Hansen Dam Golf Course', 'hansen-dam-golf-course', '12201'),
+    # GolfCourse('Hansen Dam Golf Course', 'hansen-dam-golf-course', '12201'),
     # GolfCourse('Wilson Golf Course', 'wilson-golf-course', '12204'),
     # GolfCourse('Harding Golf Course', 'harding-golf-course', '12202'),
 ]
